c9c3aac441742a05d7979da36a1bd58a596faa01
Sparks <sparks@wuffs.net>
2024-04-07 22:13:29 -0400

    Update

--- docs/conf.py
+++ docs/conf.py
@@ -17,9 +17,9 @@
 
 # -- Project information -----------------------------------------------------
 
-project = "Basic Sphinx Example Project"
-copyright = "2022, Read the Docs core team"
-author = "Read the Docs core team"
+project = "Scoutify"
+copyright = "2024, Scoutify core dev team"
+author = "RScoutify core team"
 
 
 # -- General configuration ---------------------------------------------------
